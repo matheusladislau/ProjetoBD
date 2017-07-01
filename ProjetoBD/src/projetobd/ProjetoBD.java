ecf1e2ebe2dffb9359cc7e01695520ad4b5e6c3a
Matheus <m>
2017-07-01 15:38:43 -0300

    2017-07-01 [15:38] Segundo commit

--- ProjetoBD/src/projetobd/ProjetoBD.java
+++ ProjetoBD/src/projetobd/ProjetoBD.java
@@ -14,7 +14,8 @@ public class ProjetoBD {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        System.out.println("funfando");
         // TODO code application logic here
     }
     
