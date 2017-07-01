4ddccae0ef9436905a66fcd535b7a6187fa7e4eb
Matheus <m>
2017-07-01 15:43:07 -0300

    2017-07-01 [15:43] Acrescentado classe pessoa

---
+++ ProjetoBD/src/projetobd/Pessoa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetobd;
+/**
+ *
+ * @author Matheus
+ */
+    public class Pessoa{
+    int idPessoa;
+    String nome;
+    public Pessoa(int idPessoa,String nome){
+        this.idPessoa=idPessoa;
+        this.nome=nome;
+    }
+}
